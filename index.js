ff37601a74bade1258ed7de860d303a0557b00bd
Danilov_G <gleb22d@mail.ru>
2022-11-27 22:14:19 +0200

    spa

---
+++ index.js
@@ -0,0 +1,4 @@
+import { HelloWorld } from "./components/HelloWorld.js";
+const body = document.querySelector('body');
+const helloWorld = new HelloWorld(body);
+helloWorld.render();
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(express.static('.'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '.', 'index.html'));
+});
 
 app.listen(port, function () {
 	console.log(`Server listening port ${port}`);
