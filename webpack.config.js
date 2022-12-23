66d108a5b91f943e7a09c80107f0dc6405efe656
Danilov_G <gleb22d@mail.ru>
2022-12-23 23:32:51 +0200

    changePorts

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     ],
     devServer: {
         proxy: {
-            '/api': 'http://127.0.0.1:3333'
+            '/api': 'http://127.0.0.1:3456'
         },
         historyApiFallback: true
     }
